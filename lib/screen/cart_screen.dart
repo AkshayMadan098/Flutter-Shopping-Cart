b64692eb44dd6f330336b585f4339f2f34fa6be4
Akshay Madan <40893683+silentkiller12345@users.noreply.github.com>
2022-06-13 22:14:12 +0530

    shopping cart code added

---
+++ lib/screen/cart_screen.dart
@@ -0,0 +1,281 @@
+import 'package:badges/badges.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_shopping_cart/model/cart_model.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:provider/provider.dart';
+
+import 'package:flutter_shopping_cart/provider/cart_provider.dart';
+import 'package:flutter_shopping_cart/helper/db_helper.dart';
+
+
+class CartScreen extends StatefulWidget {
+  const CartScreen({Key? key}) : super(key: key);
+
+  @override
+  _CartScreenState createState() => _CartScreenState();
+}
+
+class _CartScreenState extends State<CartScreen> {
+
+  DBHelper? dbHelper = DBHelper();
+
+  _toastInfo(String info) {
+    Fluttertoast.showToast(msg: info, toastLength: Toast.LENGTH_LONG);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final cart  = Provider.of<CartProvider>(context);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Shopping Cart'),
+        centerTitle: true,
+        actions: [
+          Center(
+            child: Badge(
+              badgeContent: Consumer<CartProvider>(
+                builder: (context, value , child){
+                  return Text(value.getCounter().toString(),style: TextStyle(color: Colors.white));
+                },
+
+              ),
+              animationDuration: Duration(milliseconds: 300),
+              animationType: BadgeAnimationType.slide,
+              child: Icon(Icons.shopping_bag_outlined),
+            ),
+          ),
+          SizedBox(width: 20.0)
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Column(
+          children: [
+            FutureBuilder(
+                future:cart.getData() ,
+                builder: (context , AsyncSnapshot<List<Cart>> snapshot){
+                  if(snapshot.hasData){
+
+                    if(snapshot.data!.isEmpty){
+                      return Align(
+                        alignment: Alignment.center,
+                        child: Column(
+                          children: [
+                            Image(
+                              image: AssetImage('images/empty_cart.png'),
+                            ),
+                            SizedBox(height: 20,),
+                            Text('Your cart is empty ' ,style: Theme.of(context).textTheme.headline5),
+                            SizedBox(height: 20,),
+                            Text('Explore products and shop your\nfavourite items' , textAlign: TextAlign.center ,style: Theme.of(context).textTheme.subtitle2)
+
+                          ],
+                        ),
+                      );
+                    }else {
+                      return Expanded(
+                        child: ListView.builder(
+                            itemCount: snapshot.data!.length,
+                            itemBuilder: (context, index){
+                              return Card(
+                                child: Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Column(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                    children: [
+                                      Row(
+                                        mainAxisAlignment: MainAxisAlignment.start,
+                                        crossAxisAlignment: CrossAxisAlignment.center,
+                                        mainAxisSize: MainAxisSize.max,
+                                        children: [
+                                          Image(
+                                            height: 100,
+                                            width: 100,
+                                            image: NetworkImage(snapshot.data![index].image.toString()),
+                                          ),
+                                          SizedBox(width: 10,),
+                                          Expanded(
+                                            child: Column(
+                                              mainAxisAlignment: MainAxisAlignment.start,
+                                              crossAxisAlignment: CrossAxisAlignment.start,
+                                              children: [
+                                                Row(
+                                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                                  children: [
+                                                    Text(snapshot.data![index].productName.toString() ,
+                                                      style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
+                                                    ),
+                                                    InkWell(
+                                                        onTap: (){
+                                                          dbHelper!.delete(snapshot.data![index].id!);
+                                                          cart.removerCounter();
+                                                          cart.removeTotalPrice(double.parse(snapshot.data![index].productPrice.toString()));
+                                                        },
+                                                        child: Icon(Icons.delete))
+                                                  ],
+                                                ),
+
+                                                SizedBox(height: 5,),
+                                                Text(snapshot.data![index].unitTag.toString() +" "+r"$"+ snapshot.data![index].productPrice.toString() ,
+                                                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
+                                                ),
+                                                SizedBox(height: 5,),
+                                                Align(
+                                                  alignment: Alignment.centerRight,
+                                                  child: InkWell(
+                                                    onTap: (){
+
+
+                                                    },
+                                                    child:  Container(
+                                                      height: 35,
+                                                      width: 100,
+                                                      decoration: BoxDecoration(
+                                                          color: Colors.green,
+                                                          borderRadius: BorderRadius.circular(5)
+                                                      ),
+                                                      child:  Padding(
+                                                        padding: const EdgeInsets.all(4.0),
+                                                        child: Row(
+                                                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                                          children: [
+                                                            InkWell(
+                                                                onTap: (){
+
+                                                                  int quantity =  snapshot.data![index].quantity! ;
+                                                                  int price = snapshot.data![index].initialPrice!;
+                                                                  quantity--;
+                                                                  int? newPrice = price * quantity ;
+
+                                                                  if(quantity > 0){
+                                                                    dbHelper!.updateQuantity(
+                                                                        Cart(
+                                                                            id: snapshot.data![index].id!,
+                                                                            productId: snapshot.data![index].id!.toString(),
+                                                                            productName: snapshot.data![index].productName!,
+                                                                            initialPrice: snapshot.data![index].initialPrice!,
+                                                                            productPrice: newPrice,
+                                                                            quantity: quantity,
+                                                                            unitTag: snapshot.data![index].unitTag.toString(),
+                                                                            image: snapshot.data![index].image.toString())
+                                                                    ).then((value){
+                                                                      newPrice = 0 ;
+                                                                      quantity = 0;
+                                                                      cart.removeTotalPrice(double.parse(snapshot.data![index].initialPrice!.toString()));
+                                                                    }).onError((error, stackTrace){
+                                                                      print(error.toString());
+                                                                    });
+                                                                  }
+
+                                                                },
+                                                                child: Icon(Icons.remove , color: Colors.white,)),
+                                                            Text( snapshot.data![index].quantity.toString(), style: TextStyle(color: Colors.white)),
+                                                            InkWell(
+                                                                onTap: (){
+                                                                  int quantity =  snapshot.data![index].quantity! ;
+                                                                  int price = snapshot.data![index].initialPrice!;
+                                                                  quantity++;
+                                                                  int? newPrice = price * quantity ;
+
+                                                                  dbHelper!.updateQuantity(
+                                                                      Cart(
+                                                                          id: snapshot.data![index].id!,
+                                                                          productId: snapshot.data![index].id!.toString(),
+                                                                          productName: snapshot.data![index].productName!,
+                                                                          initialPrice: snapshot.data![index].initialPrice!,
+                                                                          productPrice: newPrice,
+                                                                          quantity: quantity,
+                                                                          unitTag: snapshot.data![index].unitTag.toString(),
+                                                                          image: snapshot.data![index].image.toString())
+                                                                  ).then((value){
+                                                                    newPrice = 0 ;
+                                                                    quantity = 0;
+                                                                    cart.addTotalPrice(double.parse(snapshot.data![index].initialPrice!.toString()));
+                                                                  }).onError((error, stackTrace){
+                                                                    print(error.toString());
+                                                                  });
+                                                                },
+                                                                child: Icon(Icons.add , color: Colors.white,)),
+
+                                                          ],
+                                                        ),
+                                                      ),
+                                                    ),
+                                                  ),
+                                                )
+                                              ],
+                                            ),
+                                          )
+
+                                        ],
+                                      )
+                                    ],
+                                  ),
+                                ),
+                              );
+                            }),
+                      );
+                    }
+
+                  }
+                  return Text('') ;
+                }),
+            Consumer<CartProvider>(builder: (context, value, child){
+              return Visibility(
+                visible: value.getTotalPrice().toStringAsFixed(2) == "0.00" ? false : true,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    children: [
+                      PriceWidget(title: 'Sub Total', value: r'$'+value.getTotalPrice().toStringAsFixed(2),),
+                      PriceWidget(title: 'Discout 5%', value: r'$'+'20',),
+                      PriceWidget(title: 'Total', value: r'$'+value.getTotalPrice().toStringAsFixed(2),),
+                      Container(
+                        height: 50,
+                        width: double.infinity,
+                        child: RaisedButton(
+                          child: Text(
+                            "CHECKOUT",
+                            style: Theme.of(context).textTheme.button!.copyWith(
+                              color: Colors.white,
+                            ),
+                          ),
+                          onPressed: () {
+                            _toastInfo("Succesfully purchased");
+                          },
+                          color: Colors.blue,
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              );
+            })
+          ],
+        ),
+      ) ,
+    );
+  }
+}
+
+
+class PriceWidget extends StatelessWidget {
+  final String title , value ;
+  const PriceWidget({required this.title, required this.value});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 4),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(title , style: Theme.of(context).textTheme.subtitle2,),
+          Text(value.toString() , style: Theme.of(context).textTheme.subtitle2,)
+        ],
+      ),
+    );
+  }
+}
